d6c7b6861e667f2fcd9ea963a9ce9fda8fc27f89
lazy <2276282419@qq.com>
2021-11-25 17:52:11 +0800

    feat: html

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -2,9 +2,8 @@
 mkdir -p nginx/www
 mkdir -p nginx/conf
 if [ ! -f nginx/conf/nginx.conf ]; then
-    echo '' > nginx/conf/nginx.conf
+    curl -o nginx/conf/nginx.conf https://raw.githubusercontent.com/weblazy/docker/master/nginx/nginx.conf
 fi
 mkdir -p nginx/conf.d
 mkdir -p nginx/logs
-cd nginx
-docker run --net=host --name nginx-1 -v $PWD/www:/www -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/logs:/logs -v $PWD/conf.d:/etc/nginx/conf.d  -d nginx  
+docker run --net=host --name nginx-1 -v $PWD/nginx/www:/www -v $PWD/nginx/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/nginx/logs:/logs -v $PWD/nginx/conf.d:/etc/nginx/conf.d  -d nginx  
